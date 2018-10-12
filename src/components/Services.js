9b685b7b7c6d9248e49881d49d51b55804cf5458
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2018-10-11 23:51:26 -0300

    feat/fix: fix service section add resposiveness
    split service section into declarative code, separated from the data.
    add responsiveness to the section, and change image formats from png to svg.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import socialNetwork from '../img/service-social-network.png'
+import serviceData from '../utils/ServicesData'
 
 const Services = () => (
     <section>
@@ -7,16 +7,13 @@ const Services = () => (
             <span className='title-line'></span><h1 className='section-title'>Serviços</h1>
         </header>
         <div id='services'>
-            <div className='service-card'>
-                <img src={socialNetwork} alt='Two person icon' className='service-icon'></img>
-                <h3 className='service-title'>Mídias Sociais</h3>
-                <p className='service-description'>Realizamos toda a gestão de mídias sociais, desde SEO, Marketing Digital, até a criação e produção de conteúdo.</p>
-            </div>
-            <div className='service-card'>
-                <img src={socialNetwork} alt='Two person icon' className='service-icon'></img>
-                <h3 className='service-title'>Mídias Sociais</h3>
-                <p className='service-description'>Realizamos toda a gestão de mídias sociais, desde SEO, Marketing Digital, até a criação e produção de conteúdo.</p>
-            </div>
+            {serviceData.map((service) => (
+                <div className='service-card'>
+                    <img src={service.icon} alt='Two person icon' className='service-icon'></img>
+                    <h3 className='service-title'>{service.type}</h3>
+                    <p className='service-description'>{service.text}</p>
+                </div>
+            ))}
         </div>
     </section>
 )
---
+++ src/utils/ServicesData.js
@@ -0,0 +1,53 @@
+import socialNetwork from '../img/service-social-network.svg'
+import eCommerce from '../img/service-ecommerce.svg'
+import development from '../img/service-development.svg'
+import dashboard from '../img/service-dashboard.svg'
+import outsourcing from '../img/service-outsourcing.svg'
+import apps from '../img/service-apps.svg'
+import startup from '../img/service-startup.svg'
+import consulting from '../img/service-consulting.svg'
+
+const serviceData = [
+    {
+        type: 'Mídias Sociais',
+        icon: socialNetwork,
+        text: 'Realizamos toda a gestão de mídias sociais, desde SEO, Marketing Digital, até a criação e produção de conteúdo.'
+    },
+    {
+        type: 'eCommerce',
+        icon: eCommerce,
+        text: 'Preparado para tornar sua loja física em virtual? A HiveSys te dá uma ajuda! Desenvolvemos sites eCommerce para você começar a vender para novos clientes.'
+    },
+    {
+        type: 'Desenvolvimento',
+        icon: development,
+        text: 'Desenvolvemos sites e produtos institucionais sob medida. Aplicamos em nossos projetos técnicas de desenvolvimento ágil e de constante interação com o cliente.'
+    },
+    {
+        type: 'Dashboards',
+        icon: dashboard,
+        text: 'Precisa de uma forma de visualizar seus resultados? Desenvolvemos dashboards com gráficos sob medida para compreender melhor sua empresa e auxiliar a traçar metas.'
+    },
+    {
+        type: 'Outsourcing',
+        icon: outsourcing,
+        text: 'Atualmente empresas de diversos segmentos têm aderido à flexibilização em seu modelo de trabalho. Optar por um serviço de outsourcing de TI, pode ser muito mais vantajoso do que ter um departamento inteiro dedicado a isso.'
+    },
+    {
+        type: 'Apps e Sites',
+        icon: apps,
+        text: 'Seja para computador, smartphone ou tablet, sites responsivos é um requisito importantíssimo e obrigatório atualmente. Todos nossos sites operam em todos estes dispositivos.'
+    },
+    {
+        type: 'Startup',
+        icon: startup,
+        text: 'Nem só de desenvolvimento e fábrica de software vive a HiveSys. Temos um setor responsável pela criação, pesquisa e desenvolvimento de novos produtos para o mercado. Você possui uma ideia e não sabe como tira-la do papel? Vamos conversar!'
+    },
+    {
+        type: 'Consultoria',
+        icon: consulting,
+        text: 'Sua infraestrutura de rede ou seu sistema está defasado? Realizamos consultoria em sua infraestrutura rede de computadores, assim como nos sistemas da sua empresa. Nosso objetivo é apresentar um melhor cenário, mais tecnológico e eficiente para sua organização.'
+    }
+]
+
+export default serviceData
