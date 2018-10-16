ce6462d626389696727b7b775ea498a56f9c5eae
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2018-10-16 15:28:30 -0300

    feat: implement partners section
    add slider showing partners logo with an infinite loop between these images.
    Responsiveness needs to be fixed.

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,11 +1,21 @@
 import React from 'react'
-//import partnersLogo from '../utils/PartnersLogo'
+import partnersLogo from '../utils/PartnersLogo'
+import '../css/index.css'
 
 const Partners = () => (
     <div id='partners'>
         <header className='section-header header-center'>
             <h1 className='section-title about-heading'>Parceiros</h1><span id='about-line' className='title-line'></span>
         </header>
+        <div className='slider'>
+            <div className='slide'>
+                {partnersLogo.map((data, key) => (
+                    <div key={key} className='partner'>
+                        <img src={data} alt=''></img>
+                    </div>
+                ))}
+            </div>
+        </div>
     </div>
 )
 
