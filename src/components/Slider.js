13bb2127ed2ac46505fdf18ef83c0996ae57c0cd
Wender Xavier <wender.zacariasxavier@gmail.com>
2018-10-21 10:51:50 -0300

    fix: slider transition
    add automatic slider transition using timeInterval. Slider transition is
    not smooth as in the previous website.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react'
-import Slide2 from '../utils/slides/Slide02'
+import slides from '../utils/Slide'
 
 class Slider extends Component {
+    state = {
+        currentSlide: slides[0]
+    }
+
+    changeSlide(prevSlide, nextSlide, slideId) {
+        prevSlide.classList.remove('bullet-active')
+        nextSlide.classList.add('bullet-active')
+        this.setState({
+            currentSlide: slides[slideId]
+        })
+    }
+
+    componentDidMount() {
+        let slide0 = document.getElementById('slide-id-0')
+        let slide1 = document.getElementById('slide-id-1')
+        slide0.classList.add('bullet-active')
+        slide0.addEventListener('click', ()=> {
+            this.changeSlide(slide1, slide0, 0)
+        })
+
+        slide1.addEventListener('click', ()=> {
+            this.changeSlide(slide0, slide1, 1)
+        })
+
+        setInterval(()=> {
+            this.changeSlide(slide1, slide0, 0)
+        }, 5000)
+
+        setInterval(()=> {
+            this.changeSlide(slide0, slide1, 1)
+        }, 10000)
+
+        /*
+        
+        document
+          .querySelectorAll('ul.submenu_1.menushow, ul.submenu_1 .menushow')
+          .forEach(function(el) {
+            el.classList.remove('menushow');
+          })
+        
+        
+          
+        <ul class="submenu_1 menushow">
+          <li class="sub">
+              <ul class="submenu_2 menushow">
+                  <li>
+                      <ul class="submenu_2 menushow">
+                          <li>
+                              <ul class="submenu_2 menushow">
+                                  <li></li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </li>
+                  <li></li>
+              </ul>
+          </li>
+        </ul>
+        */
+
+    }
+
     render() {
+        const { currentSlide } = this.state
         return (
             <section id='slider'>
-                <Slide2 />
+                {currentSlide}
                 <div id='slider-selector'>
-                    <div className='slide-bullet'><span className='slider-btn'></span></div>
-                    <div className='slide-bullet'><span className='slider-btn'></span></div>
+                    {slides.map((slide, key) => (
+                        <div id={`slide-id-${key}`} key={key} className='slide-bullet'><span className='slider-btn'></span></div>
+                    ))}
                 </div>
             </section>
         )
--- src/utils/Slide.js
+++ src/utils/Slide.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import Slide1 from './slides/Slide01'
 import Slide2 from './slides/Slide02'
 
-export default slides = [Slide1, Slide2]
+const slides = [<Slide1 />, <Slide2 />]
+
+export default slides
